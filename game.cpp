4eb549b2f999aa6a4122a24c51dc136e57a7436e
denisjackman <moro1965>
2016-05-17 15:57:56 +0100

    fixing the game code

--- game.cpp
+++ game.cpp
@@ -13,8 +13,8 @@ int main ()
 {
   	cout << " -- Game Version 1.0 (Test) -- " << endl;
 	cout << " --- Starting ---" << endl;
-	cout << " One Dice roll only Vasily -- " << str(Dice()) << endl;
-	cout << " Distance to target Vasily -- " << str(Distance(1,0,10,0)) << endl;
+	cout << " One Dice roll only Vasily -- " << Dice() << endl;
+	cout << " Distance to target Vasily -- " << Distance(1,0,10,0) << endl;
 	cout << " --- Ending ---" << endl;
   	return 0;
 }
