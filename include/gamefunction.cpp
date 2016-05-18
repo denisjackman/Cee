4cd30dba58ab08f4317ad2ad6b440807c402a731
denisjackman <moro1965>
2016-05-18 10:22:42 +0100

    Added SDL window code to the mix

--- include/gamefunction.cpp
+++ include/gamefunction.cpp
@@ -1,3 +1,4 @@
+#include <SDL2/SDL.h>
 #include <cstdlib>
 #include <ctime>
 #include <cmath>
@@ -18,4 +19,34 @@ int Distance(double oX0, double oY0, double tX1, double tY1)
     double result = 0;
     result = sqrt((tX1 - oX0)*(tX1 - oX0) + (tY1 - oY0)*(tY1 - oY0));
     return int(result);
+}
+
+bool GameInit()
+{
+    //Initialization flag
+    bool result = true;
+
+    //Initialize SDL
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+    {
+        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+        result = false;
+    }
+    else
+    {
+        //Create window
+        gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        if( gWindow == NULL )
+        {
+            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+            result = false;
+        }
+        else
+        {
+            //Get window surface
+            gScreenSurface = SDL_GetWindowSurface( gWindow );
+        }
+    }
+
+    return result;
 }
