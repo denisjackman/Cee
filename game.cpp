9dd3434094604ba063fa40a7c6670d8b76c12519
denisjackman <moro1965>
2016-05-18 13:14:53 +0100

    fixing variable typo

--- game.cpp
+++ game.cpp
@@ -47,7 +47,7 @@ bool GameInit()
         {
             //Get window surface
             gScreenSurface = SDL_GetWindowSurface( gWindow );
-            uint32_t WHITE = SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF );
+            uint32_t WHITE = SDL_MapRGB( gScreenSurface->format, 0xFF, 0xFF, 0xFF );
         }
     }
     return result;
