93b49528eeec535d200ea4e0907f89c702ee3e5c
denisjackman <moro1965>
2016-05-23 17:40:18 +0100

    Fixed the problem with the Insult Generator

--- include/gamefunction.h
+++ include/gamefunction.h
@@ -3,6 +3,5 @@
 
 int DiceRoll(int rolls = 1, int sides = 6);
 int DistanceToTarget(double oX0, double oY0, double tX1, double tY1);
-char *InsultGenerator();
-
+std::string InsultGenerator();
 #endif
--- lesson/scrap.cpp
+++ lesson/scrap.cpp
@@ -1,35 +1,21 @@
-#include <cstdlib>
-#include <ctime>
-#include <cmath>
 #include <string>
 #include <iostream>
+#include "../include/gamefunction.h"
 
 using namespace std;
 
-int DiceRoll(int rolls = 1, int sides = 6)
-{
-	int result = 0;
-	srand(time(NULL));
-	for ( int loop =0; loop < rolls; loop = loop + 1)
-	{
-        	result += rand() % sides + 1;
-	}
-	return result;
-}
-
-int DistanceToTarget(double oX0, double oY0, double tX1, double tY1)
-{
-    double result = 0;
-    result = sqrt((tX1 - oX0)*(tX1 - oX0) + (tY1 - oY0)*(tY1 - oY0));
-    return int(result);
-}
-
-string InsultGenerator()
+string Insult()
 {
     string result = "Thou ";
     string Column1[]={"artless","bawdy","beslubbering","bootless","churlish","cockered","clouted","craven","currish","dankish","dissembling","droning","errant","fawning","fobbing","froward","frothy","gleeking","goatish","gorbellied","impertinent","jarring","loggerheaded","lumpish"};
     string Column2[]={"base-court","bat-fowling","beef-witted","beetle-headed","boil-brained","clapper-clawed","clay-brained","common-kissing","crook-pated","dismal-dreaming","dizzy-eyed","doghearted","dread-bolted","earth-vexing","elf-skinned","fat-kidneyed","fen-sucked","flap-mouthed","fly-bitten","folly-fallen","fool-born","full-gorged","guts-griping","half-faced","hasty-witted"};
     string Column3[]={"apple-john","baggage","barnacle","bladder","boar-pig","bugbear","bum-bailey","canker-blossom","clack-dish","clotpole","coxcomb","codpiece","death-token","dewberry","flap-dragon","flax-wench","flirt-gill","foot-licker","fustilarian","giglet","gudgeon","haggard","harpy","hedge-pig""horn-beast"};
-    result += Column1[DiceRoll(1,23)] + " " + Column2[DiceRoll(1,24)] + " " +Column3[DiceRoll(1,23)] +"!";
+    result += Column1[DiceRoll(1,24)] + " " + Column2[DiceRoll(1,25)] + " " +Column3[DiceRoll(1,24)] +"!";
     return result;
+}
+
+int main(int argc, char *argv[])
+{
+    cout << Insult() << endl;
+    return 0;
 }
