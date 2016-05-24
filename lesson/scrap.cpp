0fbc37769003a68af9cc6042af7dbd0d3706840c
denisjackman <moro1965>
2016-05-24 15:11:56 +0100

    Fixed more issues

--- lesson/scrap.cpp
+++ lesson/scrap.cpp
@@ -5,13 +5,5 @@
 using namespace std;
 int main(int argc, char *argv[])
 {
-    curs_set(0);
-    refresh();
-    printw("Welcome to Rogue\nPress any key to continue\nPress 'q' or 'Q' to quit at any time\n");
-    nPlayerInput = getch();
-    clear();
-
-    curs_set(1);
-    endwin();
-
+    return 0;
 }
