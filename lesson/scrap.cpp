7d1a5a068da33680fab0a5c25d89cd531ad404b9
denisjackman <moro1965>
2016-05-24 14:20:54 +0100

    Fixed ncurses

--- lesson/scrap.cpp
+++ lesson/scrap.cpp
@@ -1,9 +1,29 @@
 #include <string>
 #include <iostream>
+#include <ncurses.h>
 
 using namespace std;
-int main (int argc, char* args[] )
+int main(int argc, char *argv[])
 {
+         initscr();
 
-    return 0;
+    if(has_colors() == FALSE)
+    {
+                endwin();
+        printf("Your terminal does not support color\n");
+        exit(1);
+    }
+
+    start_color();
+    init_pair(1, COLOR_RED, COLOR_BLACK);
+
+    WINDOW *testwin = newwin (10, 10, 0, 0);
+    box (testwin, 0, 0);
+    wrefresh (testwin);
+
+        attron (COLOR_PAIR (1));
+    mvwprintw (testwin, 0, 0, "%s", "Hello");
+    attroff (COLOR_PAIR (1));
+
+    wrefresh (testwin);
 }
