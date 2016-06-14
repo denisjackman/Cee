d253ac9f8533fbb3e912dbf47bc6d6fea67e1f5c
denisjackman <moro1965>
2016-06-14 13:35:04 +0100

    updated the SDL pixel plotter

--- lesson/SDLPixPlot.cpp
+++ lesson/SDLPixPlot.cpp
@@ -1,29 +1,52 @@
 #include <stdlib.h>
-
 #include <SDL2/SDL.h>
 
 #define WINDOW_WIDTH 600
+#define WINDOW_HEIGHT 600
+
 
 int main(void) {
     SDL_Event event;
     SDL_Renderer *renderer;
     SDL_Window *window;
-    int i;
+
+    bool GameLoop = true;
 
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_CreateWindowAndRenderer(WINDOW_WIDTH, WINDOW_WIDTH, 0, &window, &renderer);
+    //
+    // SDL_CreateWindowAndRenderer(Width, Height, Window Flags, Target Window, Target Renderer)
+    //
+    // https://wiki.libsdl.org/SDL_CreateWindowAndRenderer
+    //
+    SDL_CreateWindowAndRenderer(WINDOW_WIDTH, WINDOW_HEIGHT, 0, &window, &renderer);
+    //
+    // SDL_SetRenderDrawColor(Target, Red, Green, Blue, Alpha)
+    //
+    // https://wiki.libsdl.org/SDL_SetRenderDrawColor
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
     SDL_RenderClear(renderer);
+    //
+    // SDL_SetRenderDrawColor(Target, Red, Green, Blue, Alpha)
+    //
+    // https://wiki.libsdl.org/SDL_SetRenderDrawColor
     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-    for (i = 0; i < WINDOW_WIDTH; ++i)
+
+    for (int i = 0; i < WINDOW_WIDTH; ++i)
     {
+        //
+        // SDL_RenderDrawPoint(Target Renderer, X position , Y position)
+        //
+        // https://wiki.libsdl.org/SDL_RenderDrawPoint
+        //
         SDL_RenderDrawPoint(renderer, i, i);
     }
     SDL_RenderPresent(renderer);
-    while (1)
+    while (GameLoop)
     {
         if (SDL_PollEvent(&event) && event.type == SDL_QUIT)
-            break;
+        {
+            GameLoop = false;
+        }
     }
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
