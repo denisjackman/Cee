407a8c2b29076f5481c8ff03dee28364316b8a7f
denisjackman <moro1965>
2016-06-20 13:20:26 +0100

    still fixing new media load routine

--- game.cpp
+++ game.cpp
@@ -23,84 +23,10 @@ SDL_Window*     gWindow         = NULL;
 SDL_Surface*    gScreenSurface  = NULL;
 SDL_Surface*    gDisplaySurface = NULL;
 
-
-bool GameInitialise()
-{
-    DebugModeInitialise();
-    //Initialization flag
-    bool result = true;
-    //Initialize SDL
-    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-    {
-        Print("ERROR:SDL could not initialize! SDL_Error: ");
-        result = false;
-    }
-    else
-    {
-        //Create window
-        gWindow = SDL_CreateWindow( SCREEN_TITLE, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-        if( gWindow == NULL )
-        {
-            Print("ERROR:Window could not be created! SDL_Error: ");
-            result = false;
-        }
-        else
-        {
-            //Get window surface
-            gScreenSurface = SDL_GetWindowSurface( gWindow );
-            WHITE = SDL_MapRGB( gScreenSurface->format, 0xFF, 0xFF, 0xFF );
-        }
-    }
-
-    return result;
-}
-
-void GameTerminate()
-{
-    //Deallocate surface
-    SDL_FreeSurface( gDisplaySurface );
-    gDisplaySurface = NULL;
-    //Destroy window
-    SDL_DestroyWindow( gWindow );
-    gWindow = NULL;
-    //Quit SDL subsystems
-    SDL_Quit();
-    DebugModeTerminate();
-}
-
-
-bool LoadMedia(char *loadMedia)
-{
-    bool result = true;
-    gDisplaySurface = loadSurface( "files/stretch.bmp" );
-    if (gDisplaySurface == NULL)
-    {
-        Print ("ERROR:Unable to load image " + string(SDL_GetError()) + " SDL Error ");
-        result = false;
-    }
-    return result;
-}
-
-SDL_Surface* loadSurface( string path)
-{
-    SDL_Surface* optimaizedSurface = NULL;
-    SDL_Surface* loadedSurface = SDL_LoadBMP(path.c_str() );
-    if (loadedSurface == NULL)
-    {
-        Print("Unable to load image " + path.c_str() + "! SDL Error: "+ SDL_GetError()+ "\n");
-    }
-    else
-    {
-        optimaizedSurface = SDL_ConvertSurface( LoadedSurface, gScreenSurface=>format, NULL);
-        if (optimaizedSurface == NULL)
-        {
-            Print("Unable to optimize image " + path.c_str() + "! SDL Error: "+ SDL_GetError()+ "\n")
-        }
-   		SDL_FreeSurface( loadedSurface );
-    }
-    return optimaizedSurface
-}
-
+bool GameInitialise();
+bool GameTerminate();
+SDL_Surface* loadSurface(string path);
+bool LoadMedia(char *loadMedia);
 
 int main (int argc, char* args[] )
 {
@@ -148,4 +74,81 @@ int main (int argc, char* args[] )
 	GameTerminate();
 	Print(" --- Ending ---");
   	return 0;
+}
+
+
+bool GameInitialise()
+{
+    DebugModeInitialise();
+    //Initialization flag
+    bool result = true;
+    //Initialize SDL
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+    {
+        Print("ERROR:SDL could not initialize! SDL_Error: ");
+        result = false;
+    }
+    else
+    {
+        //Create window
+        gWindow = SDL_CreateWindow( SCREEN_TITLE, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        if( gWindow == NULL )
+        {
+            Print("ERROR:Window could not be created! SDL_Error: ");
+            result = false;
+        }
+        else
+        {
+            //Get window surface
+            gScreenSurface = SDL_GetWindowSurface( gWindow );
+            WHITE = SDL_MapRGB( gScreenSurface->format, 0xFF, 0xFF, 0xFF );
+        }
+    }
+
+    return result;
+}
+
+void GameTerminate()
+{
+    //Deallocate surface
+    SDL_FreeSurface( gDisplaySurface );
+    gDisplaySurface = NULL;
+    //Destroy window
+    SDL_DestroyWindow( gWindow );
+    gWindow = NULL;
+    //Quit SDL subsystems
+    SDL_Quit();
+    DebugModeTerminate();
+}
+
+SDL_Surface* loadSurface( string path)
+{
+    SDL_Surface* optimaizedSurface = NULL;
+    SDL_Surface* loadedSurface = SDL_LoadBMP(path.c_str() );
+    if (loadedSurface == NULL)
+    {
+        Print("Unable to load image " + path.c_str() + "! SDL Error: "+ SDL_GetError()+ "\n");
+    }
+    else
+    {
+        optimaizedSurface = SDL_ConvertSurface( LoadedSurface, gScreenSurface=>format, NULL);
+        if (optimaizedSurface == NULL)
+        {
+            Print("Unable to optimize image " + path.c_str() + "! SDL Error: "+ SDL_GetError()+ "\n")
+        }
+   		SDL_FreeSurface( loadedSurface );
+    }
+    return optimaizedSurface
+}
+
+bool LoadMedia(char *loadMedia)
+{
+    bool result = true;
+    gDisplaySurface = loadSurface( "files/stretch.bmp" );
+    if (gDisplaySurface == NULL)
+    {
+        Print ("ERROR:Unable to load image " + string(SDL_GetError()) + " SDL Error ");
+        result = false;
+    }
+    return result;
 }
