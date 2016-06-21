c49a2dced7cd87e7470859fc924cf0b040ce07f6
denisjackman <moro1965>
2016-06-21 10:57:53 +0100

    fixing extern variables

--- include/SDLEngine.cpp
+++ include/SDLEngine.cpp
@@ -19,20 +19,6 @@
 #include "gamefunction.h"
 
 using namespace std;
-// Variables
-extern const int       SCREEN_WIDTH;
-extern const int       SCREEN_HEIGHT;
-extern const char      *SCREEN_TITLE;
-extern const int       PROGRAM_TIMER;
-extern const char      *VERSION;
-extern const char      *NAME_PROGRAM;
-extern string          MEDIAFILE;
-extern SDL_Window*     gWindow;
-extern SDL_Surface*    gScreenSurface;
-extern SDL_Surface*    gDisplaySurface;
-extern SDL_Surface*    gStretchedSurface;
-extern SDL_Renderer*   gRenderer;
-extern SDL_Texture*    gTexture;
 
 // Functions Code
 bool GameInitialise()
--- include/SDLEngine.h
+++ include/SDLEngine.h
@@ -3,6 +3,20 @@
 
 using namespace std;
 
+// Variables
+extern const int       SCREEN_WIDTH;
+extern const int       SCREEN_HEIGHT;
+extern const char      *SCREEN_TITLE;
+extern const int       PROGRAM_TIMER;
+extern const char      *VERSION;
+extern const char      *NAME_PROGRAM;
+extern string          MEDIAFILE;
+extern SDL_Window*     gWindow;
+extern SDL_Surface*    gScreenSurface;
+extern SDL_Surface*    gDisplaySurface;
+extern SDL_Surface*    gStretchedSurface;
+extern SDL_Renderer*   gRenderer;
+extern SDL_Texture*    gTexture;
 
 
 // Function Headers
