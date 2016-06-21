02f8bdbacf3ea3be1f2696ea1cd78adecc927d6d
denisjackman <moro1965>
2016-06-21 11:09:05 +0100

    still fixing extern variables

--- game.cpp
+++ game.cpp
@@ -15,27 +15,11 @@
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_mixer.h>
 
-using namespace std;
-
-// Variables
-const int       SCREEN_WIDTH        = 640;
-const int       SCREEN_HEIGHT       = 480;
-const char      *SCREEN_TITLE       = "Game Project";
-const int       PROGRAM_TIMER       = 2000;
-const char      *VERSION            = "V2.02.00";
-const char      *NAME_PROGRAM       = "Game";
-string          MEDIAFILE           = "files/texture.png";
-uint32_t        WHITE               = NULL;
-SDL_Window*     gWindow             = NULL;
-SDL_Surface*    gScreenSurface      = NULL;
-SDL_Surface*    gDisplaySurface     = NULL;
-SDL_Surface*    gStretchedSurface   = NULL;
-SDL_Renderer*   gRenderer           = NULL;
-SDL_Texture*    gTexture            = NULL;
 // Custom Headers
 #include "include/gamefunction.h"
 #include "include/SDLEngine.h"
 
+using namespace std;
 
 //Main code
 int main (int argc, char* args[] )
