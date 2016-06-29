2e30c6a24678b431634c0c0bdc871218bf87d6b3
denisjackman <moro1965>
2016-06-29 15:15:34 +0100

    more XML madness

--- Library/lesson/xmlexample.cpp
+++ Library/lesson/xmlexample.cpp
@@ -4,8 +4,24 @@
 using namespace std;
 using namespace tinyxml2;
 
-bool Test()
+bool Test1();
+bool Test2();
+bool Test3();
+bool Test4();
+bool Test5();
+
+int main()
 {
+   //Test1();
+   //Test2();
+   //Test3();
+   //Test4();
+   Test5();
+}
+
+bool Test1()
+{
+   // This reads an XML document
    XMLDocument doc;
 
    XMLError eResult = doc.LoadFile("../../files/example1.xml");
@@ -16,7 +32,68 @@ bool Test()
    return true;
 }
 
-int main()
+bool Test2()
 {
-   Test();
+   // This reads an XML document
+   XMLDocument doc;
+
+   XMLError eResult = doc.LoadFile("../../files/example2.xml");
+   if (eResult != XML_SUCCESS) return false;
+   const char* hello = doc.FirstChildElement("poetry")->FirstChildElement("verse")->GetText();
+   cout << hello << endl;
+
+   return true;
 }
+
+bool Test3()
+{
+   // This reads an XML document
+   XMLDocument doc;
+
+   XMLError eResult = doc.LoadFile("../../files/example3.xml");
+   if (eResult != XML_SUCCESS) return false;
+
+   const char* hello = doc.FirstChildElement("shapes")->FirstChildElement("circle")->Attribute("name");
+   cout << hello << endl;
+    return true;
+}
+
+bool Test4()
+{
+   // This reads an XML document
+   XMLDocument doc;
+   XMLError eResult = doc.LoadFile("../../files/books.xml");
+   if (eResult != XML_SUCCESS)
+   {
+        return false;
+   }
+   const char* hello = doc.FirstChildElement("MyApp")->FirstChildElement("Messages")->FirstChildElement("Welcome")->GetText();
+   const char* farewell = doc.FirstChildElement("MyApp")->FirstChildElement("Messages")->FirstChildElement("Farewell")->GetText();
+   const char* info = doc.FirstChildElement("MyApp")->FirstChildElement("Windows")->FirstChildElement("Window")->Attribute("name");
+   cout << hello << " : " << farewell << " : " << info <<  endl;
+   return true;
+}
+
+bool Test5()
+{
+    // This reads an XML document and iterates through the child elements of the
+    XMLDocument doc;
+    XMLError eResult = doc.LoadFile("../../files/books.xml");
+    if (eResult != XML_SUCCESS)
+    {
+        return false;
+    }
+    XMLElement *levelElement = doc.FirstChildElement("catalog");
+    for (XMLElement* child = levelElement->FirstChildElement(); child != NULL; child = child->NextSiblingElement())
+    {
+        // do something with each child element
+        cout << child->Attribute("id") << " : " ;
+        cout << child->FirstChildElement("genre")->GetText() << " : ";
+        cout << child->FirstChildElement("title")->GetText() << " (";
+        cout << child->FirstChildElement("author")->GetText() << ")";
+        cout << endl;
+    }
+    return true;
+}
+
+
